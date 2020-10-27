2684ab53e42257835a339f0994eb0f301e22e6b9
Andrea Santana <andrea.santana0222@gmail.com>
2020-10-26 21:40:13 -0300

    Friend List filtering for user id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_004138) do
+ActiveRecord::Schema.define(version: 2020_10_26_025637) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_10_26_004138) do
     t.string "twitter"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
